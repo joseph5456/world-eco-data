f19cab361c29830eb2269e9486de8720f45e550d
An Pham <anpham@cttdvnatl.org>
2023-10-28 16:04:57 -0400

    styling and source

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 function About() {
   return (
-    <div>
+    <div className='about-page'>
       <h2>About Page</h2>
-      <p>Our goal is to display land data on chloropleth maps</p>
+      <p>Our goal is to display land data from the United Nations on chloropleth maps</p>
+      <h2>Our source:</h2>
+      <a href="http://data.un.org/Default.aspx">http://data.un.org/Default.aspx</a>
     </div>
   );
 }
